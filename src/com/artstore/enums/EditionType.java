41bba30356d30872ea6b48a2f5f8e6607b928a1e
James S <jas812000@gmail.com>
2025-03-25 09:33:51 -0500

    Enums updated with names and getter

--- src/com/artstore/enums/EditionType.java
+++ src/com/artstore/enums/EditionType.java
@@ -1,4 +1,28 @@
+// This file is part of the ArtInventoryTransaction application, specifically the enums package.
 package com.artstore.enums;
 
+/**
+ * Enum representing edition types for prints (e.g., canvas, paper, or photo).
+ */
 public enum EditionType {
-}
+    CANVAS("CANVAS"),
+    PAPER("PAPER"),
+    PHOTO("PHOTO");
+
+    // Holds the name of the edition type, immutable for each enum constant.
+    private final String editionTypeName;
+
+    // Constructor to accept the string value
+    EditionType(String editionTypeName) {
+        this.editionTypeName = editionTypeName;
+    } // End constructor
+
+    // Getter method to retrieve the edition type name
+    public String getEditionTypeName() {
+        return editionTypeName;
+    } // End getEditionTypeName method
+} // End Enum
+
+
+
+
--- src/com/artstore/enums/Material.java
+++ src/com/artstore/enums/Material.java
@@ -1,4 +1,45 @@
+// This file is part of the ArtInventoryTransaction application, specifically the enums package.
 package com.artstore.enums;
 
+/**
+ * Enum representing materials used in sculptures (e.g., stone, metal, ceramic, wood, glass, etc.).
+ */
 public enum Material {
-}
+    STONE("STONE"),
+    METAL("METAL"),
+    WOOD("WOOD"),
+    CLAY("CLAY"),
+    CERAMIC("CERAMIC"),
+    GLASS("GLASS"),
+    RESIN("RESIN"),
+    PLASTIC("PLASTIC"),
+    WAX("WAX"),
+    ICE("ICE"),
+    BONE("BONE"),
+    PAPER("PAPER"),
+    FIBERGLASS("FIBERGLASS"),
+    PLASTER("PLASTER"),
+    CONCRETE("CONCRETE"),
+    FOUND_OBJECTS("FOUND OBJECTS"),
+    FOAM("FOAM"),
+    TEXTILE("TEXTILE"),
+    LEATHER("LEATHER"),
+    WIRE("WIRE"),
+    SAND("SAND"),
+    CARBON_FIBER("CARBON FIBER"),
+    THREE_D_PRINTED_MATERIALS("3D PRINTED MATERIALS"),
+    BIODEGRADABLE_MATERIALS("BIODEGRADABLE MATERIALS");
+
+    // Holds the name of the material type, immutable for each enum constant.
+    private final String materialName;
+
+    // Constructor to accept the string value
+    Material(String materialName) {
+        this.materialName = materialName;
+    } // End constructor
+
+    // Getter method to retrieve the material name
+    public String getMaterialName() {
+        return materialName;
+    } // End getMaterialName method
+} // End Enum
