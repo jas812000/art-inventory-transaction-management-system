0cdfd9e0e8bcfe74016bcde3dab40acf560beb7e
James S <jas812000@gmail.com>
2025-03-24 19:48:54 -0500

    Initial Commit

--- src/ArtInventoryTransction/HomePanel.java
+++ src/ArtInventoryTransction/HomePanel.java
@@ -31,7 +31,7 @@ public class HomePanel extends JPanel {
 
     // Path to image resource relative to the root of the project
     private static final String IMAGE_PATH =
-            System.getProperty("user.dir") + "/images/graffiti-abstract.jpg";
+            System.getProperty("user.dir") + "/src/images/graffiti-abstract.jpg";
 
     // declare an instance of Image
     private Image backgroundImage;
